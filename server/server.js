bbc3ead3c58477d8df3008b92e0079cd541cf324
KirillBotskov <103040337+KirillBotskov@users.noreply.github.com>
2022-09-28 11:10:12 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+
 const app = express()
 const db = require('./dbconnect')
 const port = process.env.port || 5000;
@@ -9,42 +10,35 @@ app.use(cors())
 app.use(bodyParser.json())
 app.set('json spaces', 2)
 
-// http://localhost:5000/keemia/user/authentication/:address/:pass
-app.get('/keemia/user/authentication/:addressLog/:passLog', db.UserAuthentication);
-
-// http://localhost:5000/keemia/update/user/status/:address/:pass
-app.put('/keemia/update/user/status/:address/:pass', db.UpdateUserStatusLogin);
-
-// http://localhost:5000/keemia/update/user/status/exit
-app.put('/keemia/update/user/status/exit', db.UpdateUserStatusExit);
+// http://localhost:5000/keemia/user/authentication
+app.post('/keemia/user/authentication', db.UserAuthentication);
 
 // http://localhost:5000/keemia/user/registration
 app.post('/keemia/user/registration', db.UserRegistration);
 
 // http://localhost:5000/keemia/get/chemelement
-app.get('/keemia/get/chemelement', db.GetChemElement);
+app.get('/keemia/get/chemelement',db.verityToken, db.GetChemElement);
 
 // http://localhost:5000/keemia/get/element/:name
-app.get('/keemia/get/element/:name', db.SearchElementbyName);
+app.get('/keemia/get/element/:name',db.verityToken, db.SearchElementbyName);
 
 // http://localhost:5000/keemia/get/all/elements
-app.get('/keemia/get/all/elements', db.GetAllElements);
+app.get('/keemia/get/all/elements',db.verityToken, db.GetAllElements);
 
 // http://localhost:5000/keemia/add/element
-app.post('/keemia/add/element', db.AddNewElement);
+app.post('/keemia/add/element',db.verityToken, db.AddNewElement);
 
 // http://localhost:5000/keemia/update/element/status/:addidUpd
-app.put('/keemia/update/element/status/:addidUpd', db.UpdateElementStatus);
+app.put('/keemia/update/element/status/:addidUpd',db.verityToken, db.UpdateElementStatus);
 
 // http://localhost:5000/keemia/add/elements/spendtable
-app.post('/keemia/add/elements/spendtable', db.AddElementToSpendTable);
+app.post('/keemia/add/elements/spendtable',db.verityToken, db.AddElementToSpendTable);
 
 // http://localhost:5000/keemia/deletechemlist
-app.get('/keemia/get/deleted/elements', db.GetDeletedElements);
+app.get('/keemia/get/deleted/elements',db.verityToken, db.GetDeletedElements);
 
 // http://localhost:5000/keemia/clear/deleted/elements/table/:id
-app.delete('/keemia/clear/deleted/elements/table/:id', db.ClearDeletedElementsTable);
-
+app.delete('/keemia/clear/deleted/elements/table/:id',db.verityToken, db.ClearDeletedElementsTable);
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
